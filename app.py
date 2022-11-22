225387b190fcccab60918801b53f34e643492ff8
Rajeev2604 <rajeevrc007@gmail.com>
2022-11-22 18:06:08 +0000

    All files added

---
+++ app.py
@@ -0,0 +1,29 @@
+from flask import Flask, jsonify, request, app, render_template,url_for
+import numpy as np
+import pandas as pd
+import json
+import pickle
+
+app = Flask(__name__)
+#Load the model
+regmodel = pickle.load(open('regmodel.pkl','rb'))
+scalar=pickle.load(open('scaling.pkl','rb'))
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+
+
+@app.route('/predict', methods=['Post'])
+def predict():
+    data = [float(x) for x in request.form.values()]
+    final_input=scalar.transform(np.array(data).reshape(1,-1))
+    print(final_input)
+    output = regmodel.predict(final_input)[0]
+    return render_template("home.html", prediction_text="The House prediction is: {}".format(output))
+
+
+if __name__=="__main__":
+    app.run(debug=True)
+
